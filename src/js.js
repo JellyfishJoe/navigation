7bc8362d988ca94b5dc3d41bca5aae6067a70e99
JellyfishJoe <jellyfishjoe5@gmail.com>
2018-10-30 21:20:09 -0700

    asd

--- src/js.js
+++ src/js.js
@@ -1,3 +1,4 @@
+/*
 $(document).ready(function(){
 	$('#menu > li > ul').css({
 		'display':'none',
@@ -12,4 +13,49 @@ $(document).ready(function(){
 		console.log('yikers');
 	});
 });
+*/
 
+/*
+//This is retarded but kinda cool
+$(document).ready(function(){
+
+	$('.depth0 li').click(function(){
+		$('.depth0 li').append('<ul><li>Cool</li></ul>');
+	});
+
+});
+*/
+
+$(document).ready(function(){
+
+	var classNames = [];
+
+	$("li").each(function(){
+		var depth = 'depth' + ($(this).parents("li").length) + 'li';
+		var siblings = $(this).siblings().length;
+     	$(this).addClass(depth); 
+     	classNames.push(depth)
+     	var siblings.
+	});
+
+	console.log(`siblings = ${siblings}`);
+
+	console.log(classNames);
+
+	console.log()
+
+	$('a').click(function(){
+
+		console.log($(this).parents('li').length - 1);
+
+		if($(this).hasClass(classNames[0])){
+			//console.log(true);
+		}
+		/*
+		$('.depth0li').append('<ul><li>Cool</li></ul>');
+		$('.depth0li ul').addClass('depth1');
+		$('.depth0li ul li').addClass('depth1li');
+		*/
+	});
+
+});
